e5db549781ebbb15cad4c42bd4c68930f4d9611b
Madhusudhan Rao <70625227+madhusudhanrao-ppm@users.noreply.github.com>
2024-11-28 09:53:52 +0530

    Update CallT2S.sql

--- AI-for-Healthcare/CallT2S.sql
+++ AI-for-Healthcare/CallT2S.sql
@@ -17,7 +17,7 @@ DECLARE
     v_planetext := TRANSLATE(v_planetext, '~!@#$%^&*()_+=\{}[]:”;’<,>./?',' ') ;  
     -- invoke Text to Speech PL/SQL function
     file_name := SPEECH_SYNTHESIZESPEECH_UPLOADFILE (v_planetext,v_voice );
-    file_name := 'https://objectstorage.us-phoenix-1.oraclecloud.com/n/oradbclouducm/b/medical_transcripts/o/Speech/'||file_name; 
+    file_name := 'https://objectstorage.us-phoenix-1.oraclecloud.com/n/tenancyname/b/bucketname/o/Speech/'||file_name; 
     l_text := l_text||'<br/> <audio controls preload> <source src="'||file_name||'" type="audio/mpeg"> </audio><img src="'||doctor_img||'" width="250px"/>'; 
     -- Output HTML to Richtext Input page item
     :P68_AUDIO := l_text;
