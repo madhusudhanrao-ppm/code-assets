f6fe1f4085c195bbe8e9f4fd6428243fc9cb5d89
Madhusudhan Rao <70625227+madhusudhanrao-ppm@users.noreply.github.com>
2025-01-20 17:49:49 +0530

    Update CallT2S.sql

--- AI-for-Healthcare/CallT2S.sql
+++ AI-for-Healthcare/CallT2S.sql
@@ -3,7 +3,7 @@ DECLARE
     l_text varchar2(4000);  
     file_name varchar2(200); 
     v_voice varchar2(100) := :P68_VOICE; 
-    v_planetext varchar2(32000) :=  :P68_PROMPT;
+    v_plaintext varchar2(32000) :=  :P68_PROMPT;
      doctor_img varchar2(200); 
 
  BEGIN
@@ -16,7 +16,7 @@ DECLARE
     -- clean up input text from any special chars
     v_planetext := TRANSLATE(v_planetext, '~!@#$%^&*()_+=\{}[]:”;’<,>./?',' ') ;  
     -- invoke Text to Speech PL/SQL function
-    file_name := SPEECH_SYNTHESIZESPEECH_UPLOADFILE (v_planetext,v_voice );
+    file_name := SPEECH_SYNTHESIZESPEECH_UPLOADFILE (v_plaintext,v_voice );
     file_name := 'https://objectstorage.us-phoenix-1.oraclecloud.com/n/tenancyname/b/bucketname/o/Speech/'||file_name; 
     l_text := l_text||'<br/> <audio controls preload> <source src="'||file_name||'" type="audio/mpeg"> </audio><img src="'||doctor_img||'" width="250px"/>'; 
     -- Output HTML to Richtext Input page item
