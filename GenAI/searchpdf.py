39ad406229275fa4b5a2dcb5229b04618be52c8d
Madhusudhan Rao <70625227+madhusudhanrao-ppm@users.noreply.github.com>
2025-06-11 13:55:18 +0530

    Update searchpdf.py

--- GenAI/searchpdf.py
+++ GenAI/searchpdf.py
@@ -12,14 +12,14 @@ pages = loader.load_and_split()
 llm = OCIGenAI(
     model_id="cohere.command",
     service_endpoint="https://inference.generativeai.us-chicago-1.oci.oraclecloud.com",
-    compartment_id="ocid1.compartment.oc1..aaaaaaaaud6tkdn6n23cbvc4hexs6n4hggetkwo4viqyneyroixcmj54u32q",
+    compartment_id="ocid1.compartment.oc1..aaaaaaaaud6tkdnXixcmj54u32q",
     model_kwargs={"temperature": 0.7, "top_p": 0.75, "max_tokens": 1000}
 )
 
 embeddings = OCIGenAIEmbeddings(
     model_id="cohere.embed-english-light-v3.0",
     service_endpoint="https://inference.generativeai.us-chicago-1.oci.oraclecloud.com",
-    compartment_id="ocid1.compartment.oc1..aaaaaaaaud6tkdn6n23cbvc4hexs6n4hggetkwo4viqyneyroixcmj54u32q",
+    compartment_id="ocid1.compartment.oc1..aaaaaaaaud6tXXmj54u32q",
 )
 
 vectorstore = Chroma.from_documents(
@@ -43,6 +43,5 @@ chain = (
     | StrOutputParser()
 )
 
-print(chain.invoke("What is Amrit Kaal"))
-#print(chain.invoke("What is India's fiscal deficit"))
+print(chain.invoke("What is India's fiscal deficit")) 
  
