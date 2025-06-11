fb0e6cc395e9f025ed5255451e84c0d2c45a9380
Madhusudhan Rao <70625227+madhusudhanrao-ppm@users.noreply.github.com>
2025-06-11 23:02:38 +0530

    Create shard2.py

---
+++ OGDD/pages/shard2.py
@@ -0,0 +1,38 @@
+import cx_Oracle
+import oracledb
+import streamlit as st 
+from streamlit_navigation_bar import st_navbar
+import pandas as pd
+import numpy as np
+
+st.set_page_config(
+    page_title = "Hello",
+    page_icon = "🦜🔗 Bank Customers"    
+)
+
+st.title("Bank Customers in Shard2");
+
+# Shard connection details
+username = 'docuser'
+password = 'YourPassword'
+# dsn = '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=your_shard_host)(PORT=your_shard_port))(CONNECT_DATA=(SERVICE_NAME=your_shard_service_name)))'
+dsn = '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=shardhost2)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=shard2)))'
+
+# Establish a connection
+connection = cx_Oracle.connect(username, password, dsn, encoding="UTF-8")
+
+# Create a cursor
+
+with connection.cursor() as cursor:
+    sql = """select custid, FirstName, LastName, Class, CustProfile from bank_customers"""
+    df = pd.DataFrame(cursor.execute(sql))
+    df.columns = ['ID','First Name', 'Last Name', 'Class', 'Profile']
+st.dataframe(df)
+
+st.title("Bank Products");
+
+with connection.cursor() as cursor2:
+    sql = """select ProductId, Product_Name, Description from bank_products"""
+    df = pd.DataFrame(cursor2.execute(sql))
+    df.columns = ['ProductId','Product_Name', 'Description']
+st.dataframe(df)
